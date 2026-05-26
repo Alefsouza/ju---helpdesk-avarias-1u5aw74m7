78df253c487a22595d7cefaf97d6eadfdd45746f
Memory Git <memory@git.local>
2026-05-26 13:22:05 -0300

    feat: Add Secretária Técnica user role

--- src/components/admin/gestao-equipe-modal.tsx
+++ src/components/admin/gestao-equipe-modal.tsx
@@ -211,6 +211,7 @@ export function GestaoEquipeModal({
                       <SelectItem value="sos">SOS</SelectItem>
                       <SelectItem value="juridico">Jurídico</SelectItem>
                       <SelectItem value="sinistro">Sinistro</SelectItem>
+                      <SelectItem value="secretaria_tecnica">Secretária Técnica</SelectItem>
                     </SelectContent>
                   </Select>
                   <FormMessage />
